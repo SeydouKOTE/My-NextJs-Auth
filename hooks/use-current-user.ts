3fa5c2747ad94f183941ed834c445e8accc30b3a
koteseydou8@gmail.com <koteseydou8@gmail.com>
2025-04-24 15:00:19 +0100

    22: responsive

--- hooks/use-current-user.ts
+++ hooks/use-current-user.ts
@@ -1,6 +1,7 @@
-import { useSession } from "next-auth/react";
+import {useSession} from "next-auth/react";
 
 export const useCurrentUser = () => {
-  const { data, status } = useSession();
-  return { user: data?.user, status };
-};
+    const session = useSession();
+
+    return session.data?.user;
+}
